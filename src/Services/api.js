5d76fc41002984fa53634ab1dbd78fd52a122bb4
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-02-02 19:50:39 +0530

    removed api key

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "1b1db8accb2bf9b21667146d9223aa24";
+const API_KEY = ""; // Add Secret API Key from TMDB
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
